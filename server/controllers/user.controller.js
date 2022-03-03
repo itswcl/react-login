dffae69dca086be03dfce712da2e1f7abde3fa43
weicheng lee <z82111712@gmail.com>
2022-03-03 13:24:11 -0800

    update controller for 1. create 2.login and 3. logout

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -12,21 +12,55 @@ module.exports = {
     create: (req, res) => {
         User.create(req.body)
             .then(user => {
-                res.json({ user: user });
-                console.log("success register")
+                // res.json({ user: user });
+                // console.log("success register")
+                // we do the same when we registered a user we giving JWT token and cookie stored
+                const userToken = jwt.sign({
+                    id: user_id
+                }, process.env.ACCESS_TOKEN_SECRET)
+
+                res.cookie(
+                    "usertoken",
+                    userToken,
+                    secret,
+                    { httpOnly: true })
+                    .json({
+                        message: "success log in"
+                    })
             })
             .catch(err => res.status(400).json({ message: "❌❌❌ Something Wrong create user ❌❌❌", error: err }))
     },
 
-    login: (req, res) => {
-        const payload = { id: user_id };
+    // remember to add async for await use
+    login: async (req, res) => {
+        // await the User to findOne see the user exist
+        const user = await User.findOne({ email: req.body.email })
+        if (user === null) return res.sendStatus(400);
 
-        const userToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, /* optional {expiresIn: "1h"}*/)
+        // if we pass the email test now we need to check the password
+        // compare the bcrypt hash password and the one we stored
+        const correctPassword = await bcrypt.compare(req.body.password, user.password);
+
+        // if password is correct we giving the token and cookie for the user
+        // const payload = { id: user_id }; add direct to userToken
+        // JWT token store
+        const userToken = jwt.sign({ id: user_id }, process.env.ACCESS_TOKEN_SECRET, /* optional {expiresIn: "1h"}*/)
         res.json({ userToken: userToken })
 
-        res.cookie("myCookie", "myData", { httpOnly: true }).json({
-            message: "This response has a cookie"
-        })
+        // cookie store
+        res.cookie(
+            "userToken",
+            userToken,
+            secret,
+            { httpOnly: true })
+            .json({
+                message: "success log in"
+            })
+    },
+
+    logout: (req, res) => {
+        res.clearCookie('userToken');
+        res.sendStatus(200;)
     }
 }
 
