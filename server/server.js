e645a9fdad18887385d3571a5bcc48aa0f8aaa8b
weicheng lee <z82111712@gmail.com>
2022-03-02 23:24:32 -0800

    add dotenv

--- server/server.js
+++ server/server.js
@@ -9,6 +9,10 @@ app.use(cors(), express.json(), express.urlencoded({extended: true}))
 // config
 require('./config/mongoose.config')(DB)
 // route
+require('./routes/user.route')
+// env key
+require('dotenv').config();
+const key = process.env.SECRET_KEY;
 
 app.listen(PORT, () => {
     console.log(`server up on port: ${PORT}`)
