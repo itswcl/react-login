3242d26fb7d50a0adad80a689012119db67048e0
weicheng lee <z82111712@gmail.com>
2022-03-02 18:08:11 -0800

    initial server connection

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 8000;
+const DB = "user_db";
+
+app.use(cors(), express.json(), express.urlencoded({extended: true}))
+
+// config
+require('./config/mongoose.config')(DB)
+// route
+
+app.listen(PORT, () => {
+    console.log(`server up on port: ${PORT}`)
+})
