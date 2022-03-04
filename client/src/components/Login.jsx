a6ae26fe5463be2aea6000236093a134bff21d37
weicheng lee <z82111712@gmail.com>
2022-03-04 08:33:29 -0800

    update frontend login with test info

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,24 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Login = () => {
+// get the log in en error we pass from Main
+const Login = ({ login, error }) => {
+    // receive from user enter user detail
+    const [detail, setDetail] = useState({
+        email: "",
+        password: ""
+    })
+
+    // user submit we pass to login function from main
     const handleSubmit = (e) => {
+        // prevent refresh
         e.preventDefault();
-        console.log("Submit")
+
+        login(detail);
     }
 
     return (
         <form onSubmit={handleSubmit}>
             <h1 className='text-center'>Log In</h1>
+            { error !== "" ? (
+                <p className='text-center text-danger'>{error}</p>
+            ) : ""}
             <div className='mb-3 row'>
                 <label htmlFor="" className='col-sm-2 col-form-label'>Email</label>
                 <div className='col-sm-10'>
-                    <input type="email" name="email" id="email" className='form-control' />
+                    {/* user input onChange with ... and e.target.value */}
+                    <input type="email" name="email" id="email" className='form-control' onChange={e => setDetail({ ...detail, email: e.target.value })} value={detail.email} />
                 </div>
             </div>
             <div className='mb-3 row'>
                 <label htmlFor="password" className='col-sm-2 col-form-label'>Password</label>
                 <div className='col-sm-10'>
-                    <input type="password" name="password" id="password" className='form-control' />
+                    {/* detail input onChange with ... and e.target.value */}
+                    <input type="password" name="password" id="password" className='form-control' onChange={e => setDetail({ ...detail, password: e.target.value })} value={detail.password} />
                 </div>
             </div>
             <div className='row'>
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Login from './Login';
 
 const Main = () => {
-    const [user, setUser] = useState({
+    const [currentUser, setCurrentUser] = useState({
         firstName: "",
         lastName: "",
         email: "",
@@ -10,15 +10,45 @@ const Main = () => {
 
     const [error, setError] = useState("");
 
+    const admin = {
+        firstName: "test",
+        lastName: "test",
+        email: "test@gmail.com",
+        password: "password"
+    }
+
+    const login = (detail) => {
+        // console.log(detail)
+
+        if (detail.email === admin.email && detail.password === admin.password) {
+            setCurrentUser({
+                ...currentUser,
+                firstName: admin.firstName,
+                email: admin.email
+            })
+            console.log("login success")
+        } else {
+            setError("login failed")
+        }
+    }
+
+    const logout = () => {
+        setCurrentUser({
+            ...currentUser,
+            firstName: "",
+            email: ""
+        })
+    }
+
     return (
         <>
-            {(user.email !== "") ? (
+            {(currentUser.email !== "") ? (
                 <div>
-                    <h1>Welcome, <span>{user.firstName}</span></h1>
-                    <button className='btn btn-success'>Logout</button>
+                    <h1>Welcome, <span>{currentUser.firstName}</span></h1>
+                    <button className='btn btn-success' onClick={logout}>Logout</button>
                 </div>
             ) : (
-                <Login />
+                <Login login={login} error={error} />
             )}
         </>
     )
